a7c45a35c6120da38832cbdaf92d367003e39a1d
imota <mota.icaro@gmail.com>
2017-11-18 15:51:08 -0500

    Created a Game class for specific games to inherit from

--- main.py
+++ main.py
@@ -1,15 +1,42 @@
 import gym
 
-environments = ['MsPacman-v0', 'Enduro-v0', 'CartPole-v0']
+class Game:
+    def prepare(self, environment_name):
+        self.environment = environment_name
+        self.env = gym.make(self.environment)
+        self.isDone = False
+        self.reset()      
 
-env = gym.make(environments[0])
+    def reset(self):
+        self.observation = self.env.reset()
+
+    def select_action(self):
+        return self.env.action_space.sample()
+
+    def run(self):
+        self.env.render()
+        action = self.select_action()
+        observation, reward, done, info = self.env.step(action)
+
+        self.observation = observation
+        self.isDone = done
+
+class Enduro(Game):
+    def __init__(self):
+        self.prepare('Enduro-v0')
+
+class MsPacman(Game):
+    def __init__(self):
+        self.prepare('MsPacman-v0')
+
+class CartPole(Game):
+    def __init__(self):
+        self.prepare('CartPole-v0')
+
+game = MsPacman()
 for i_episode in range(20):
-    observation = env.reset()
+    game.reset()
     for t in range(100):
-        env.render()
-        print(observation)
-        action = env.action_space.sample()
-        observation, reward, done, info = env.step(action)
-        if done:
-            print("Episode finished after {} timesteps".format(t+1))
+        game.run()
+        if game.isDone:
             break
