f4d8835256b3c7f87f3d05f596caab2b59209c94
imota <mota.icaro@gmail.com>
2018-02-25 14:36:08 -0500

    Working on the agent, environment structure. Including ROS in the code as an example.

--- game.py
+++ game.py
@@ -1,17 +1,30 @@
-import gym
+#!/usr/bin/env python
 
-class Game:
-    def prepare(self, environment_name):
-        self.environment = environment_name
-        self.env = gym.make(self.environment)
+import gym
+import rospy
+import numpy as np
+from rospy.numpy_msg import numpy_msg
+from std_msgs.msg import String
+
+class Game(object):
+    def __init__(self):
+        rospy.init_node('environment_node', anonymous=True)
+        self.observation_pub = rospy.Publisher('observations_topic', String, queue_size=1)
+        self.rate = rospy.Rate(10)
+        pass
+
+    def prepare_environment(self, environment_name):
+        self.name = environment_name
+        self.env = gym.make(self.name)
+        self.action_space = self.env.action_space
         self.isDone = False
-        self.reset()      
+        self.reset()   
 
     def reset(self):
         self.observation = self.env.reset()
 
     def select_action(self):
-        return self.env.action_space.sample()
+        return self.action_space.sample()
 
     def run(self):
         self.env.render()
@@ -20,15 +33,27 @@ class Game:
 
         self.observation = observation
         self.isDone = done
+        self.publish_observation()
+        
+        self.rate.sleep()
+
+    def publish_observation(self):
+        self.observation_pub.publish("it works")
+
+    def publish_reward(self):
+        pass
 
 class Enduro(Game):
     def __init__(self):
-        self.prepare('Enduro-v0')
+        Game.__init__(self)
+        self.prepare_environment('Enduro-v0')
 
 class MsPacman(Game):
     def __init__(self):
-        self.prepare('MsPacman-v0')
+        super(Game, self).__init__()
+        self.prepare_environment('MsPacman-v0')
 
 class CartPole(Game):
     def __init__(self):
-        self.prepare('CartPole-v0')
+        super(Game, self).__init__()
+        self.prepare_environment('CartPole-v0')
--- main.py
+++ main.py
@@ -1,9 +1,20 @@
+#!/usr/bin/env python
+
 import game
 
-game = game.Enduro()
-for i_episode in range(20):
-    game.reset()
-    for t in range(1000):
-        game.run()
-        if game.isDone:
-            break
+class Policy(object):
+    def __init__(self, action_space):
+        self.action_space = action_space
+
+class Agent(object):
+    def __init__(self, game):
+        self.game = game
+
+if __name__ == '__main__':
+    game = game.Enduro()
+    for i_episode in range(20):
+        game.reset()
+        for t in range(1000):
+            game.run()
+            if game.isDone:
+                break
