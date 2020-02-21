f7b92582e5231d9d3ebcce683650061e699591ac
= <mota.icaro@gmail.com>
2020-02-21 12:36:36 -0300

    simplifying the main code

--- scripts/main.py
+++ scripts/main.py
@@ -4,29 +4,20 @@
 import environments
 import json
 import pandas as pd
+from datetime import datetime
 
 def write_results(episode_number):
-#    episode_number = 'Episode ' + str(i_episode)
-
-#    with open('results_pacman_sincrono.csv', 'a') as f:
-#        reward = game.get_cummulative_reward()
-#        d = {'Episode': [episode_number], 'Reward': [reward]}
-#        df = pd.DataFrame(data=d)
-#        df.to_csv(f, header=False)   
     pass
 
 if __name__ == '__main__':
     print('Starting program...')
     game = environments.get_game()
 
-    # with open('results_pinball_random.csv', 'a') as f:
-    #    d = {'Episode', 'Reward'}
-    #    df = pd.DataFrame(data=d)
-    #    df.to_csv(f, header=False)
-
     num_episodes = 1
     for i_episode in range(1):
         print('Starting episode: ' + str(i_episode + 1))
+        start_time = datetime.now()
         game.play()    
+        game_time = datetime.now() - start_time
         write_results(i_episode + 1)
     print('End of program')
